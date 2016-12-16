2920c0d6b6a1229a4503d56c979b7219802ea87e
marulitua <erwinmaruli@live.com>
2016-12-17 03:47:50 +0700

    add xmonad

---
+++ xmonad.hs
@@ -0,0 +1,19 @@
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import System.IO
+
+main = xmonad $ defaultConfig
+	{ borderWidth					= 1
+	, terminal						= "tilda"
+	, normalBorderColor		= "#cccccc"
+	, focusedBorderColor	= "#cd8b00"
+	, manageHook = manageDocks <+> manageHook defaultConfig
+	, layoutHook = avoidStruts  $  layoutHook defaultConfig
+	} `additionalKeys`
+	[ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
+	, ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s")
+	, ((0, xK_Print), spawn "scrot")
+	]
