fc49ad2ff129f2720f818d7d744f99e3177941c2
Erwin Maruli <erwinmaruli@gmail.com>
2019-02-06 09:51:05 +0800

    add script to install font

--- install_font.sh
+++ install_font.sh
@@ -8,9 +8,9 @@ mkdir -p $DIR
 
 pushd $DIR
 
-curl -fLo "Fura Code Retina Nerd Font Complete.otf" https://github.com/haasosaurus/nerd-fonts/raw/regen-mono-font-fix/patched-fonts/FiraCode/Retina/complete/Fura%20Code%20Retina%20Nerd%20Font%20Complete.otf
+#curl -fLo "Fura Code Retina Nerd Font Complete.otf" https://github.com/haasosaurus/nerd-fonts/raw/regen-mono-font-fix/patched-fonts/FiraCode/Retina/complete/Fura%20Code%20Retina%20Nerd%20Font%20Complete.otf
 
-curl -fLo "Fura Code Retina Nerd Font Complete Mono.otf" https://github.com/haasosaurus/nerd-fonts/raw/regen-mono-font-fix/patched-fonts/FiraCode/Retina/complete/Fura%20Code%20Retina%20Nerd%20Font%20Complete%20Mono.otf
+#curl -fLo "Fura Code Retina Nerd Font Complete Mono.otf" https://github.com/haasosaurus/nerd-fonts/raw/regen-mono-font-fix/patched-fonts/FiraCode/Retina/complete/Fura%20Code%20Retina%20Nerd%20Font%20Complete%20Mono.otf
 
 curl -fLo "Fura Code Retina Nerd Font Complete Mono.ttf" https://github.com/haasosaurus/nerd-fonts/raw/regen-mono-font-fix/patched-fonts/FiraCode/Retina/complete/Fura%20Code%20Retina%20Nerd%20Font%20Complete%20Mono.ttf
 
